179c60bf9b46495aa7e715b88d22178f56e49d0d
vallaripadole16 <vallaripadole16@gmail.com>
2021-04-09 19:36:08 +0530

    Initial commit

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<!-- navbar --> 
+ <div class="collapsenavbar-collapse" id="collapsibleNavbar">
+      <nav class="navbar navbar-expand-md navbar-light bg-light" >
+      <a class="navbar-brand" href="index.php" style="color : #c91628; "><b>BASIC BANKING SYSTEM</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #c91628;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #c91628;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #c91628;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #c91628;"><b>Transaction History</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/in/vallari-padole-4504b619b/" style="color : #c91628;"><b>About Us</b></a>
+              </li>
+          </div>
+       </nav> -->
